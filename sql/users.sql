d18d640d1f2ac6eee625c7a5b33057978b5a645c
Janasri S <156062455+Janasri-16@users.noreply.github.com>
2026-07-13 11:42:16 +0530

    Add database user management scripts

--- sql/users.sql
+++ sql/users.sql
@@ -1,1 +1,28 @@
--- Database Users
+-- ============================================
+-- Database User Management
+-- ============================================
+
+-- Create Read Only User
+CREATE USER 'readonly'@'%' IDENTIFIED BY 'StrongPassword123!';
+
+GRANT SELECT
+ON companydb.*
+TO 'readonly'@'%';
+
+FLUSH PRIVILEGES;
+
+SHOW GRANTS FOR 'readonly'@'%';
+
+-- Create Developer User
+CREATE USER 'developer'@'%' IDENTIFIED BY 'Developer@123';
+
+GRANT
+SELECT,
+INSERT,
+UPDATE
+ON companydb.*
+TO 'developer'@'%';
+
+FLUSH PRIVILEGES;
+
+SHOW GRANTS FOR 'developer'@'%';
